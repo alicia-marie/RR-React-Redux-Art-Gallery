965b715f08c6b0300c3c51f160bb696f31bf5d53
Alicia MacEwan-Gutierrez <alicia7749@gmail.com>
2023-06-07 18:38:48 -0700

    created art gallery

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import './App.css';
-import { useSelector, useDispatch } from 'react-redux'
+import { useSelector, useDispatch, connect } from 'react-redux'
 import { useEffect } from 'react';
 
-function App(props) {
+import {
+  fetchData,
+  nextImage,
+  prevImage,
+  setArtId,
+  reset
+} from './features/dataSlice';
+
+const mapStateToProps = state => state.data;
+
+function App({ artId }) {
   const dispatch = useDispatch()
   const data = useSelector((state) => state.data)
 
@@ -14,24 +24,31 @@ function App(props) {
     }
   }
 
+  useEffect(() => {
+    dispatch(fetchData())
+  }, [artId, dispatch])
+
   return (
     <div className="App">
       <div>
         <button onClick={() => {
-          // dispatch fetchData
+          dispatch(fetchData())
         }}>Thunk!</button>
         <button onClick={() => {
           // dispatch reset
+          dispatch(reset())
         }}>Clear</button>
         <button onClick={() => {
-          // dispatch next
+          dispatch(nextImage())
         }}>Next</button>
         <button onClick={() => {
           // dispatch prev
+          dispatch(prevImage())
         }}>Back</button>
       </div>
       <input value={ data?.artId } onChange={(e) => {
         // dispatch setArtId
+        dispatch(setArtId(e.target.value))
       }} />
       <div>
         {data?.artId}
@@ -41,4 +58,4 @@ function App(props) {
   );
 }
 
-export default App;
+export default connect(mapStateToProps)(App);
